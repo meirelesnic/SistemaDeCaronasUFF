d66e6dc56a41457df731113ea7417273b44d60ff
Leonardo Maia <leomaiapleo@gmail.com>
2024-05-24 20:55:37 -0300

    bubble new design

--- uff_caronas/lib/view/custom_widgets/messageBubble.dart
+++ uff_caronas/lib/view/custom_widgets/messageBubble.dart
@@ -5,12 +5,14 @@ class MessageBubble extends StatelessWidget {
   final String time;
   final bool isSender;
   final String username;
+  final double raio;
 
   const MessageBubble({
     required this.message,
     required this.time,
     required this.isSender,
     required this.username,
+    this.raio = 33
   });
 
   @override
@@ -18,11 +20,27 @@ class MessageBubble extends StatelessWidget {
     return Align(
       alignment: isSender ? Alignment.topRight : Alignment.topLeft,
       child: Container(
-        margin: isSender ? EdgeInsets.fromLTRB(50, 5, 10, 5) : EdgeInsets.fromLTRB(10, 5, 50, 5),
+        margin: isSender
+            ? EdgeInsets.fromLTRB(50, 5, 10, 5)
+            : EdgeInsets.fromLTRB(10, 5, 50, 5),
         padding: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
         decoration: BoxDecoration(
-          color: isSender ? Theme.of(context).colorScheme.primary : Theme.of(context).colorScheme.primaryContainer,
-          borderRadius: BorderRadius.circular(15),
+          color: isSender
+              ? Theme.of(context).colorScheme.primary
+              : Theme.of(context).colorScheme.primaryContainer,
+          borderRadius: isSender
+              ? BorderRadius.only(
+                  topLeft: Radius.circular(raio),
+                  topRight: Radius.circular(0),
+                  bottomLeft: Radius.circular(raio),
+                  bottomRight: Radius.circular(raio),
+                )
+              : BorderRadius.only(
+                  topLeft: Radius.circular(0),
+                  topRight: Radius.circular(raio),
+                  bottomLeft: Radius.circular(raio),
+                  bottomRight: Radius.circular(raio),
+                ),
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -30,22 +48,28 @@ class MessageBubble extends StatelessWidget {
             Text(
               username,
               style: Theme.of(context).textTheme.titleMedium?.copyWith(
-              color: isSender ? Theme.of(context).colorScheme.background : Theme.of(context).colorScheme.onPrimaryContainer,
-              )
+                    color: isSender
+                        ? Theme.of(context).colorScheme.background
+                        : Theme.of(context).colorScheme.onPrimaryContainer,
+                  ),
             ),
             SizedBox(height: 5),
             Text(
               message,
               style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-              color: isSender ? Theme.of(context).colorScheme.background : Theme.of(context).colorScheme.onPrimaryContainer,
-              )
+                    color: isSender
+                        ? Theme.of(context).colorScheme.background
+                        : Theme.of(context).colorScheme.onPrimaryContainer,
+                  ),
             ),
             SizedBox(height: 5),
             Text(
               time,
               style: Theme.of(context).textTheme.bodySmall?.copyWith(
-              color: isSender ? Theme.of(context).colorScheme.background : Theme.of(context).colorScheme.onPrimaryContainer,
-              )
+                    color: isSender
+                        ? Theme.of(context).colorScheme.background
+                        : Theme.of(context).colorScheme.onPrimaryContainer,
+                  ),
             ),
           ],
         ),
