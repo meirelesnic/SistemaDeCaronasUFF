088b8b6c11052e72b5473588a8fead9e6312afec
JoaoUFF <joaocavalcanti@id.uff.br>
2024-05-26 03:34:39 -0300

    Operação de para restringir a placa e ano do veiculo. Não há controle sobre o modelo do carro

--- uff_caronas/lib/controller/VeiculoController.dart
+++ uff_caronas/lib/controller/VeiculoController.dart
@@ -1,4 +1,3 @@
-
 import '../model/DAO/VeiculoDAO.dart';
 import '../model/modelos/Veiculo.dart';
 
@@ -9,7 +8,8 @@ class VeiculoController {
     veiculoDAO = VeiculoDAO();
   }
 
-  void salvarVeiculo(String modelo, String marca, String cor, int ano, String userId, String placa) {
+  void salvarVeiculo(String modelo, String marca, String cor, int ano,
+      String userId, String placa) {
     veiculoDAO?.salvarVeiculo(modelo, marca, cor, ano, userId, placa);
   }
 
@@ -17,7 +17,44 @@ class VeiculoController {
     veiculoDAO?.atualizarVeiculo(veiculo);
   }
 
-  Future<List<Veiculo>>? veiculosPorUsuarioId(String id){
+  Future<List<Veiculo>>? veiculosPorUsuarioId(String id) {
     return veiculoDAO?.recuperarVeiculosPorUsuario(id);
+  }
+
+  bool validaAno(int year) {
+    int aux = DateTime.now().year;
+    if (aux < year || year + 25 < aux) return false;
+    return true;
+  }
+
+  bool validaPlaca(String placa) {
+    if (placa.length != 7) {
+      return false;
+    }
+
+    for (int i = 0; i < 3; i++) {
+      if (!_isLetter(placa[i])) {
+        return false;
+      }
+    }
+    if (!_isLetter(placa[4]) && !_isDigit(placa[4])) {
+      return false;
+    }
+
+    for (int i = 3; i < 7; i = i + 2 - i ~/ 4) {
+      if (!_isDigit(placa[i])) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  bool _isLetter(String character) {
+    return character.toUpperCase() != character.toLowerCase();
+  }
+
+  bool _isDigit(String character) {
+    return int.tryParse(character) != null;
   }
 }
